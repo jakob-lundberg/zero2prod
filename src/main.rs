b6c38f08df928cfbe58dd0123f0d3f90d7ce25d7
Jakob Lundberg <jakob@deductivelabs.com>
2024-03-15 15:12:54 +0100

    Test form

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,6 @@ use zero2prod::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port 8000");
+    println!("Listening to port 8000");
     run(listener)?.await
 }
