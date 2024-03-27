b095d9c9eeebcbe18bf34f1971be891f4a7c3a1c
Jakob Lundberg <jakob@deductivelabs.com>
2024-03-27 10:46:21 +0100

    Chapter 4 done

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 //! main.rs
-use env_logger::Env;
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
 
     let connection_string = configuration.database.connection_string();
-    let db_pool = PgPool::connect(&connection_string)
+    let db_pool = PgPool::connect(connection_string.expose_secret())
         .await
         .expect("Failed to connect to database");
 
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -9,9 +9,24 @@ pub struct FormData {
     name: String,
 }
 
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form),
+    fields(
+        subscriber_email = %form.email,
+        subscriber_name = %form.name,
+    )
+)]
 pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> impl Responder {
-    log::info!("Saving new subscriber details in the database");
-    match sqlx::query!(
+    match insert_subscriber(&form, &db_pool).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+#[tracing::instrument(name = "Saving new subscriber details in the database", skip(form))]
+pub async fn insert_subscriber(form: &FormData, db_pool: &PgPool) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
     insert into subscriptions (id, email, name, subscribed_at)
         values ($1, $2, $3, $4)
@@ -21,16 +36,11 @@ pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) ->
         form.name,
         Utc::now(),
     )
-    .execute(db_pool.get_ref())
+    .execute(db_pool)
     .await
-    {
-        Ok(_) => {
-            log::info!("New subscriber details have been saved");
-            HttpResponse::Ok().finish()
-        }
-        Err(err) => {
-            log::error!("Failed to execute query: {:?}", err);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
 }
